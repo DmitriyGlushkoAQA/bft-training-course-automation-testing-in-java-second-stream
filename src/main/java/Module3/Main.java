4edab7543b7644f5e6de2700b9417e46ee647e07
Dmitriy Glushko <d.glushko@bft.ru>
2025-10-28 18:56:32 +0300

    Практическое задание. Модуль 3. Добавил переопределение статической переменной

--- src/main/java/Module3/Main.java
+++ src/main/java/Module3/Main.java
@@ -1,7 +1,12 @@
 package Module3;
 
+import static Module3.RecipientOfTheService.pensionAmount;
+
 public class Main {
+
     public static void main(String[] args) {
+        pensionAmount = 21233.45;
+
         RecipientOfTheService recipient = new RecipientOfTheService();
         recipient.flName = "Столяров Иван";
         recipient.workExp = 30;
